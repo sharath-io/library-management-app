0c1f192fc2bb9bc43d49738277505890fba9392f
Sharath <sharath.esr@gmail.com>
2025-05-06 21:00:10 +0530

    feat: add initial routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import { Button } from './components/ui/button'
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Dashboard from "./components/custom/dashboard/Dashboard";
+import StudentsList from "./components/custom/students-list/StudentsList";
+import AddStudent from "./components/custom/add-student/AddStudent";
+import IssueBook from "./components/custom/issue-book/IssueBook";
+import ReturnBook from "./components/custom/return-book/ReturnBook";
+import Analytics from "./components/custom/analytics/Analytics";
+import Chart from "./components/custom/chart/Chart";
+import AddBook from "./components/custom/add-book/AddBook";
+import EditBook from "./components/custom/edit-book/EditBook";
 
 const App = () => {
   return (
-    <div>
-      <Button size="lg">Click here</Button>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/addBook" element={<AddBook/>}/>
+        <Route path="/editBook" element={<EditBook/>}/>
+        <Route path="/studentsList" element={<StudentsList/>}/>
+        <Route path="/addStudent" element={<AddStudent/>}/>
+        <Route path="/issueBook" element={<IssueBook/>}/>
+        <Route path="/returnBook" element={<ReturnBook/>}/>
+        <Route path="/analytics" element={<Analytics/>}/>
+        <Route path="/chart" element={<Chart/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
